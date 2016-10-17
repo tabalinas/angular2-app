0e3e6951a065657f8ee9881e0bd1738d28800cea
Artem Tabalin <tabalinas@gmail.com>
2016-10-17 09:05:49 +0200

    Step1: Two-way data binding with @angular/forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core'
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular app</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="Name"/>
+        </div>
+        `
 })
+export class AppComponent {
+    title = 'Tour of Heroes';
 
-export class AppComponent { }
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
